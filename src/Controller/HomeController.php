8f7fe64012a2c9261fd1a0513d12a9d2fd0159c8
Yann Bouchereau <yann.bchr@gmail.com>
2022-10-07 15:20:06 +0200

    make ajax function get songs

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use SpotifyWebAPI\Session;
 use SpotifyWebAPI\SpotifyWebAPI;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,6 +13,12 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+
+    #[Route('/songs/get', name: 'app_home_get_songs')]
+    public function getSongs(): JsonResponse
     {
         $session = new Session(
             $this->getParameter('app.client_id'),
@@ -21,46 +28,45 @@ class HomeController extends AbstractController
         $session->requestCredentialsToken();
         $accessToken = $session->getAccessToken();
 
-        if (is_string($accessToken)) {
-            $api = new SpotifyWebAPI();
-            $api->setAccessToken($accessToken);
-
-            $options = [
-                'additional_types' => 'track',
-                'fields' => 'tracks.total'
-            ];
-            $totalTracks = $api->getPlaylist($this->getParameter('app.playlist_id'), $options)->tracks->total;
-
-            $limit = 50;
-            $offset = rand(0, ($totalTracks - $limit));
-            $options = [
-                'fields' => 'items.track(album(images),artists,name,preview_url)',
-                'limit' => $limit,
-                'offset' => $offset
-            ];
-            $response = $api->getPlaylistTracks($this->getParameter('app.playlist_id'), $options);
-
-            $tracks = array_values(
-                array_map(function ($t) {
-                    $t->artist = $t->track->artists[0]->name;
-                    $t->image = new \stdClass();
-                    $t->image->height = $t->track->album->images[0]->height;
-                    $t->image->url = $t->track->album->images[0]->url;
-                    $t->image->width = $t->track->album->images[0]->width;
-                    $t->title = $t->track->name;
-                    $t->url = $t->track->preview_url;
-
-                    unset($t->track);
-
-                    return $t;
-                }, array_filter($response->items, function ($t) {
-                    return !is_null($t->track->preview_url);
-                }))
-            );
+        if (!is_string($accessToken)) {
+            return new JsonResponse(['message' => 'failed to authenticate to Spotify API'], 400);
         }
+        $api = new SpotifyWebAPI();
+        $api->setAccessToken($accessToken);
 
-        return $this->render('home/index.html.twig', [
-            'tracks' => $tracks
-        ]);
+        $options = [
+            'additional_types' => 'track',
+            'fields' => 'tracks.total'
+        ];
+        $totalTracks = $api->getPlaylist($this->getParameter('app.playlist_id'), $options)->tracks->total;
+
+        $limit = 50;
+        $offset = rand(0, ($totalTracks - $limit));
+        $options = [
+            'fields' => 'items.track(album(images),artists,name,preview_url)',
+            'limit' => $limit,
+            'offset' => $offset
+        ];
+        $response = $api->getPlaylistTracks($this->getParameter('app.playlist_id'), $options);
+
+        $tracks = array_values(
+            array_map(function ($t) {
+                $t->artist = $t->track->artists[0]->name;
+                $t->image = new \stdClass();
+                $t->image->height = $t->track->album->images[0]->height;
+                $t->image->url = $t->track->album->images[0]->url;
+                $t->image->width = $t->track->album->images[0]->width;
+                $t->title = $t->track->name;
+                $t->url = $t->track->preview_url;
+
+                unset($t->track);
+
+                return $t;
+            }, array_filter($response->items, function ($t) {
+                return !is_null($t->track->preview_url);
+            }))
+        );
+
+        return new JsonResponse($tracks);
     }
 }
