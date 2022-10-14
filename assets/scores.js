f1fa1d94ade70f0b52e16c2db621e67a6df31185
Yann Bouchereau <yann.bchr@gmail.com>
2022-10-14 17:04:41 +0200

    css

--- assets/scores.js
+++ assets/scores.js
@@ -15,19 +15,21 @@ const createTable = (scores) => {
     console.log('Scores');
     console.log(scores);
     let table = document.createElement('table');
-    table.className = 'table is-fullwidth m-auto';
+    table.className = 'table is-fullwidth is-size-5 m-auto';
 
     let thead = table.createTHead();
     let rowHeader = thead.insertRow();
     let playerHeader = rowHeader.insertCell();
     let scoreHeader = rowHeader.insertCell();
+    let strongPlayerHeader = document.createElement("strong");
+    let strongScoreHeader = document.createElement("strong");
     let headerPlayerText = document.createTextNode('Player');
     let headerScoreText = document.createTextNode('Points');
 
-    rowHeader.className = 'is-selected';
-
-    playerHeader.appendChild(headerPlayerText);
-    scoreHeader.appendChild(headerScoreText);
+    strongPlayerHeader.appendChild(headerPlayerText);
+    strongScoreHeader.appendChild(headerScoreText);
+    playerHeader.appendChild(strongPlayerHeader);
+    scoreHeader.appendChild(strongScoreHeader);
 
     scores.forEach((score) => {
         let row = table.insertRow();
@@ -40,6 +42,10 @@ const createTable = (scores) => {
         scoreCell.appendChild(scoreText);
     });
     container.appendChild(table);
+}
+
+const addPlayAgainButton = () => {
+
 }
 
 export { displayScores };
